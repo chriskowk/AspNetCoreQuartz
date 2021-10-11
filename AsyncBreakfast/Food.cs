0e276c11b8730c74e5e3c1bdc87a7171f7c9e4cf
chriskowk <chriskwok@126.com>
2021-10-11 17:13:01 +0800

    add proj

---
+++ AsyncBreakfast/Food.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncBreakfast
+{
+    public abstract class Food
+    {
+        public abstract int Id { get; }
+        public abstract string Name { get; }
+    }
+
+    public class Toast : Food
+    {
+        public override int Id => 1;
+        public override string Name => "Toast";
+    }
+    
+    public class Juice : Food
+    {
+        public override int Id => 2;
+        public override string Name => "Juice";
+    }
+
+    public class Bacon : Food
+    {
+        public override int Id => 3;
+        public override string Name => "Bacon";
+    }
+
+    public class Egg : Food
+    {
+        public override int Id => 4;
+        public override string Name => "Egg";
+    }
+
+    public class Coffee : Food
+    {
+        public override int Id => 5;
+        public override string Name => "Coffee";
+    }
+}
