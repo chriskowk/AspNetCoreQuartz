fc96793cecf6da5d5324d1617fac1fd4b1473589
chriskowk <chriskwok@126.com>
2022-02-09 14:33:07 +0800

    增加使用提示

--- MessageBox/App.xaml.cs
+++ MessageBox/App.xaml.cs
@@ -17,13 +17,14 @@ namespace Jetsun.AspNetCore
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            string usage = "用法示例： MessageBox.exe 提示信息1 提示信息2 提示信息3 ......\r\n说明：提示信息若中间有空格请使用{SPACE}替换，因为空格是多个命令行参数的分隔符。";
             string msg = string.Empty;
             for (int i = 0; i < e.Args.Length; i++)
             {
                 msg += e.Args[i] + "\r\n";
             }
             msg = msg.Replace("{SPACE}", " ");
-            Message = msg;
+            Message = string.IsNullOrWhiteSpace(msg) ? usage : msg;
         }
     }
 }
